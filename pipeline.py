91baa70569b10a0d73292ae4a6b468c6e6c0c74f
Ahmed Elsharkawy <sharkawyy98@gmail.com>
2022-06-09 02:14:28 +0200

    Load data into PostgreSQL database

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from subprocess import call
+from sqlalchemy import create_engine
 
 
 def generate_sensors_data():
@@ -14,9 +15,18 @@ def transform_sensors_data(sensors_df):
     sensors_df = sensors_df.sort_values(by=['timestamp', 'sensor'], ignore_index=True)
     return sensors_df
 
+def load_to_postgres(sensors_df):
+    engine = create_engine('postgresql://root:root@pg-database:5432/sensors')
+    try:
+        sensors_df.to_sql('data', con=engine, if_exists='replace', index_label='id')
+    except Exception as e:
+        print('Error occured:', e)
+    return 'Data loaded into PostgreSQL database successfully.'
+
 
 if __name__ == "__main__":
     generate_sensors_data()
     sensors_df = extract_sensors_data('sensors_data.csv')
     sensors_df = transform_sensors_data(sensors_df)
-    print(sensors_df.head(8))
+    msg = load_to_postgres(sensors_df)
+    print(msg)
